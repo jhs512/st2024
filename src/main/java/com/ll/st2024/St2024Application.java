de57a83ca5a086a667cb262ae0996c2f97767138
jhs512 <jangka512@gmail.com>
2024-03-22 01:58:51 +0900

    work

---
+++ src/main/java/com/ll/st2024/St2024Application.java
@@ -0,0 +1,13 @@
+package com.ll.st2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class St2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(St2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/st2024/St2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.st2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class St2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
